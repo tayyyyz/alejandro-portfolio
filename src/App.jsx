185702a19e1ba6f9c3007e9177a1852dd564d6cd
Alejandro Tay <alejandroatmbrit@gmail.com>
2025-10-26 14:03:30 +0100

    Fix: corrected Header import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/header"
+import Header from "./components/Header"
 import Hero from "./components/Hero"
 import Projects from "./components/Projects";
 import Contact from "./components/Contact"
